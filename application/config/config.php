12dec332df41e524861a8ed494364dd5ebb732cb
desertfox77 <68386288+desertfox77@users.noreply.github.com>
2021-07-29 22:12:16 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/guestbook/';
+$config['base_url'] = 'http://3.221.59.234//guestbook/';
 
 /*
 |--------------------------------------------------------------------------
