b4e4395729d58ba8199cc4b9e27e19618ed95c84
desertfox77 <68386288+desertfox77@users.noreply.github.com>
2021-07-30 12:45:48 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login Go-Fit</title>
+	<title>Login Guestbook</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
